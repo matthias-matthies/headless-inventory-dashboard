0f547c865ce3334d81c9b6993634cea9fb8350af
matthias-matthies <m.matthies@uni-bremen.de>
2023-10-16 20:21:12 +0200

    Alphine und Tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+    content: [
+        "./index.html", // don't forget to add other files and directories
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-
