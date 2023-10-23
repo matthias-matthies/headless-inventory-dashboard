19efc7cc795724b0718af7b36a213cf604bdb26f
Matthias Matthies <matmat@uni-bremen.de>
2023-10-23 11:03:46 +0200

    Alpine js testing

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import './style.css'
 import Alpine from "alpinejs"
 
+// Re-usable local state
+Alpine.data('dropdown', () => ({
+    open: false,
+    toggle() {
+        this.open = ! this.open
+    }
+}))
+
+// Global state
+Alpine.store('tabs', {
+    current: 'first',
+    items: ['first', 'second', 'third'],
+})
+
+// Last so data can be registered
 Alpine.start();
